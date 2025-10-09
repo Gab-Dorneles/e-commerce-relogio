b1d21639d4782df4c547991fed5e5baac9edfc75
Gabriela Dorneles <dorneles466@gmail.com>
2025-10-09 17:04:54 -0300

    inicio tela de login

--- Frontend/e-commerce-relogio/src/app/app.ts
+++ Frontend/e-commerce-relogio/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('e-commerce-relogio');
+  protected readonly title = signal('Ominitrix');
 }
